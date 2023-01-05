582aee08520919c7c9519a42fa92f41e31620b29
LiviaDita09 <liviadita123@gmail.com>
2023-01-05 17:36:54 +0700

    Jawaban Soal 2

---
+++ database/migrations/2023_01_05_100528_create_dosens_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDosensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dosens', function (Blueprint $table) {
+            $table->increments('id_dosen', 11);
+            $table->string('nip', 20);
+            $table->string('nama_dosen', 50);
+            $table->string('foto_dosen', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dosens');
+    }
+}
